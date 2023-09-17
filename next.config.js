533c1cadc90361c347f6583a83efa104afd1439e
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-09-17 21:25:12 +0330

    ref : refactor next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimentalFeatures: {
-    reactServerComponents: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
