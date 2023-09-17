7ca4b9681a40842d2a1adff46385f4e0ec5b44f1
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-09-17 19:31:54 +0330

    ref : add react server component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimentalFeatures: {
+    reactServerComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
